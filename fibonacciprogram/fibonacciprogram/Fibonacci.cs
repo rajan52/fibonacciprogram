de708e9b3faedab59e6d126661b066a021c8663c
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-06-03 19:30:58 +0530

    Add files via upload

---
+++ fibonacciprogram/fibonacciprogram/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fibonacciprogram
+{
+   public class Fibonacci
+    {
+        public int[] FibonacciIterative(int len)
+        {
+            int[] arr = null;
+            int f1 = 0, f2 = 1, i;
+
+            if (len < 1)
+                return arr;
+            else
+            {
+                arr = new int[len];
+                arr[0] = f1;
+            }
+
+            for (i = 1; i < len; i++)
+            {
+                arr[i] = f2;
+                int next = f1 + f2;
+                f1 = f2;
+                f2 = next;
+            }
+            return arr;
+        }
+    }
+}
